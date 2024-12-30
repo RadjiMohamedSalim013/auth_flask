e7174370f4e6bfafddebf992402553354b7ac570
Radji Mohamed <radjimohamed013@gmail.com>
2024-12-30 20:05:31 +0100

    Ajout du hachage des mots de passe pour renforcer la sécurité

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -31,17 +32,20 @@ def home():
 
 
 #connexion
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-
-        user = User.query.filter(User.username == username).first()
-        if user and user.password == password:
+        # Vérifier l'utilisateur dans la base de données
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
             session['username'] = username
             return redirect(url_for('home'))
+        else:
+            return 'Nom d\'utilisateur ou mot de passe incorrect'
+
     return render_template('login.html')
 
 
@@ -56,7 +60,11 @@ def register():
         if existed :
             return "Cet utilisateur existe déjà"
         
-        new_user = User(username=username, password=password)
+        # Hacher le mot de passe avant de le sauvegarder
+        hashed_password = generate_password_hash(password)
+
+        # Ajouter le nouvel utilisateur à la base de données
+        new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
